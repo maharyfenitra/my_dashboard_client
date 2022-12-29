c0a8350fd1eb390cb31220cea34f1a88ac2fbcfc
Mahary R <maharyfenitra@gmail.com>
2022-12-29 17:16:40 +0300

    Setup theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import { useMemo } from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from 'theme';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(()=> createTheme(themeSettings(mode)),[mode])
   return (
     <div className="app">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
     </div>
   );
 }
