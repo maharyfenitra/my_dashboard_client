f9991b4b70d22c11c99341d842bc4e57261b4667
Mahary R <maharyfenitra@gmail.com>
2022-12-30 15:33:19 +0300

    NavBar and Drawer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,12 +12,53 @@ import FlexBetween from "components/FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
 import profileImage from "assets/profile.jpg"
-import { useTheme } from "@mui/material";
+import { AppBar, IconButton, InputBase, InputLabel, Toolbar, useTheme } from "@mui/material";
 
-const Navbar = () => {
+
+const Navbar = ({
+    isSideBarOpen,
+    setIsSideBarOpen
+}) => {
     const dispatch = useDispatch()
-    const theme = useTheme
-    return <div>Navbar</div>;
+    const theme = useTheme()
+    return <AppBar
+        sx={{
+            position: "static",
+            background: "none",
+            boxShadow: "none"
+        }}
+    >
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+            {/** LEFT SIDE */}
+            <FlexBetween>
+                <IconButton onClick={() =>  setIsSideBarOpen(!isSideBarOpen)}>
+                    <MenuIcon />
+                </IconButton>
+                <FlexBetween
+                    backgroundColor={theme.palette.background.alt}
+                    borderRadius="9px"
+                    gap="3rem"
+                    style={{paddingLeft: "10px"}}
+                    //p="O.1rem 1.5rem"
+                >
+                    <InputBase placeholder="Search..." />
+                    <IconButton>
+                        <Search />
+                    </IconButton>
+                </FlexBetween>
+            </FlexBetween>
+
+            {/** RIGHT SIDE */}
+            <FlexBetween gap="1.5rem">
+                <IconButton onClick={() => dispatch(setMode())}>
+                    {theme.palette.mode === 'dark' ? (<DarkModeOutlined sx={{ fontSize: "25px" }} />) : (<LightModeOutlined sx={{ fontSize: "25px" }} />)}
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlined sx={{fontSize: "25px"}}/>
+                </IconButton>
+            </FlexBetween>
+        </Toolbar>
+    </AppBar>;
 };
 
 export default Navbar;
--- src/scenes/layout/index.js
+++ src/scenes/layout/index.js
@@ -3,11 +3,23 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
+import SideBar from "components/SideBar";
 
 const Layout = () => {
-    return <Box width="100%" height="100%">
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
+    const [isSideBarOpen, setIsSideBarOpen] = useState(true)
+    return <Box display={isNonMobile ? "flex": "block"} width="100%" height="100%">
         <Box>
-            <Navbar />
+            <SideBar 
+                drawerWidth="250px"
+                isNonMobile={isNonMobile}
+                isSideBarOpen={isSideBarOpen}
+                setIsSideBarOpen={setIsSideBarOpen}
+            />
+            <Navbar 
+                isSideBarOpen={isSideBarOpen}
+                setIsSideBarOpen={setIsSideBarOpen}
+            />
             <Outlet />
         </Box>
     </Box>
